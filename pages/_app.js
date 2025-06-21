b7172dee111ab8e2f06a1da3de0e59e589ca26a3
john <mariano.rodriguez@gmail.com>
2025-06-21 17:18:37 -0400

    Fix: Remove BrowserRouter to support Next.js SSR

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import Home from './Home';
-//import Project from './Project';
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-  {/* <Route path="/" element={<Home />} />
-  <Route path="/project/:id" element={<Project />} /> */}
-</Routes>
-
-    </Router>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
