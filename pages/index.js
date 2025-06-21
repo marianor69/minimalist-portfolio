7218d0c487b61ecb2f9b58d4241989d5f83ef72a
john <mariano.rodriguez@gmail.com>
2025-06-21 17:25:57 -0400

    Fix import and use of projects data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { projects } from '../src/data';
+import { projects } from "../src/data";
+
 
 
 export default function Portfolio() {
--- src/data.js
+++ src/data.js
@@ -1,8 +1,20 @@
 export const projects = [
   {
-    id: 1,
-    title: "Sample Project",
-    description: "This is a placeholder project.",
-    image: "/images/sample.jpg"
-  }
+    title: "Caniff",
+    description: "Complete rehab of a single-family home including structural work, new roof, kitchen, bathroom, and HVAC.",
+    beforeImgs: ["/images/caniff/before1.jpg", "/images/caniff/before2.jpg"],
+    afterImgs: ["/images/caniff/after1.jpg", "/images/caniff/after2.jpg"],
+  },
+  {
+    title: "Joel",
+    description: "Major kitchen renovation with open-concept layout and modern finishes.",
+    beforeImgs: ["/images/joel/before1.jpg", "/images/joel/before2.jpg"],
+    afterImgs: ["/images/joel/after1.jpg", "/images/joel/after2.jpg"],
+  },
+  {
+    title: "Wyman",
+    description: "Finished basement renovation including a new bathroom, laundry room, and den.",
+    beforeImgs: ["/images/wyman/before1.jpg", "/images/wyman/before2.jpg"],
+    afterImgs: ["/images/wyman/after1.jpg", "/images/wyman/after2.jpg"],
+  },
 ];
